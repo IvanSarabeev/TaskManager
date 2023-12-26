3d1fbae4b87fad9997545a2f11fbb9fbb68fdea4
IvanSarabeev <ivan.sarabeev99@gmail.com>
2023-12-26 11:25:30 +0200

    feat: user authentication

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,23 @@
       {{ session()->get('success') }}  
     </div><br />
   @endif
+  @if(session()->get('error'))
+    <div class="alert alert-danger">
+      {{ session()->get('error') }}
+    </div><br />
+  @endif
 <a href="{{ route('tasks.create')}}" class="btn btn-success">
     <i class="bi bi-folder-plus"></i> ADD</a>
+    <div class="float-end">
+      {{ Auth::user()->name }}    
+      <a href="{{ route('logout') }}" class="btn btn-dark" onclick="event.preventDefault();
+       document.getElementById('logout-form').submit();">    
+        {{ __('Logout') }}    
+      </a>    
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">    
+        @csrf    
+      </form>    
+    </div>
   <table class="table table-striped">
     <thead>
         <tr>
@@ -26,13 +41,21 @@
             <td>{{$tasks->title}}</td>
             <td>{{$tasks->description}}</td>
             <td>{{$tasks->due_date}}</td>
-            <td><a href="{{ route('tasks.edit', $tasks->id)}}" class="btn btn-primary"><i class="bi bi-pencil-square"></i> Edit</a></td>
             <td>
+              @if(Auth::user()->isAdmin==1)
+                <a href="{{ route('tasks.edit', $tasks->id)}}" class="btn btn-primary">
+                  <i class="bi bi-pencil-square"></i> Edit
+                </a>
+               @endif
+              </td>
+            <td>
+              @if(Auth::user()->isAdmin==1)
                 <form action="{{ route('tasks.destroy', $tasks->id)}}" method="post">
                   @csrf
                   @method('DELETE')
                   <button class="btn btn-danger" type="submit"><i class="bi bi-x-circle"></i> Delete</button>
                 </form>
+              @endif
             </td>
         </tr>
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::resource('tasks', 'TaskController')->middleware('auth');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
