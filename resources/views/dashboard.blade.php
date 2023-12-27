c0415d4cf00f2ee460fbcd6beb4fc2b15b13b780
IvanSarabeev <ivan.sarabeev99@gmail.com>
2023-12-27 13:21:47 +0200

    style: design changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,15 +5,15 @@
         </h2>
     </x-slot>
 
-    <section class="max-w-7xl h-fit container mx-auto pt-12 pb-8">
+    <section class="max-w-7xl h-fit container mx-auto pt-12 pb-8 px-2">
         <div class="h-auto mx-auto flex flex-col gap-y-6 items-start justify-start sm:px-6 md:px-10 lg:px-14 xl:px-16 2xl:px-20 rounded-lg shadow-md bg-white text-gray-900">
             <h1 class="font-semibold text-3xl pt-8 mb-4">First 10 Records</h1>
             <table class="min-w-full divide-y divide-gray-200 overflow-x-auto shadow-md">
                 <thead class="text-base text-left font-semibold text-gray-900 bg-gray-50/5">
                     <tr>
                         <th class="py-3.5 pl-4 pr-3">№</th>
-                        <th class="py-3.5 pl-4 pr-3">Title</th>
-                        <th class="py-3.5 pl-4 pr-3">Description</th> 
+                        <th class="hidden lg:table-cell py-3.5 pl-4 pr-3">Title</th>
+                        <th class="hidden lg:table-cell py-3.5 pl-4 pr-3">Description</th> 
                         <th class="py-3.5 pl-4 pr-3">Status</th>
                     </tr>
                 </thead>
@@ -26,20 +26,14 @@
                       <td class="w-full max-w-0 sm:w-auto sm:max-w-none whitespace-nowrap px-3 py-4 text-sm font-medium text-gray-900">
                         {{$task->title}}
                         <dl class="lg:hidden font-normal">
-                          <dt class="sr-only sm:hidden">Date</dt>
-                          <dd class="sm:hidden text-gray-700 mt-1">{{$task->due_date}}</dd>
                           <dt class="sr-only">Description</dt>
-                          <dd class="text-gray-500 sm:text-gray-700 mt-1 truncate">{{$task->description}}</dd>
-                          <dt class="sr-only">Status</dt>
-                          <dd class="text-gray-500 sm:text-gray-700 mt-1 truncate">{{$task->status}}</dd>
-                          <dt class="sr-only sm:hidden">Priority</dt>
-                          <dd class="sm:hidden text-gray-500 sm:text-gray-700 mt-1 truncate capitalize">{{$task->priority}}</dd>
+                          <dd class="text-gray-500 sm:text-gray-700 mt-1 truncate">{{$task->description}}</dd>                          
                         </dl>
                       </td>
                       <td class="hidden lg:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500 truncate">
                         {{$task->description}}
                       </td>
-                      <td class="hidden md:table-cell lg:max-w-sm whitespace-nowrap px-3 py-4 text-sm text-gray-500 truncate">
+                      <td class="table-cell lg:max-w-sm whitespace-nowrap px-3 py-4 text-sm text-gray-500 truncate">
                         {{$task->status ? "Completed": "Pending"}}
                       </td>
                     </tr>
